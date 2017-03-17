1cb63b40234802c9ab8e99477cb66fe7e6a613d3
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2017-03-17 13:14:43 +0100

    update

--- application.js
+++ application.js
@@ -2,6 +2,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM loaded launching functions");
     start();
 
+    window.addEventListener("keydown", function(event) {
+        if (event.defaultPrevented) {
+            return; // Do nothing if the event was already processed
+        } else {
 
-
+            console.log("event", event);
+            // Cancel the default action to avoid it being handled twice
+            event.preventDefault();
+        }
+    }, true);
 });
+
+
+
+
+
+function start() {
+
+    console.log("hey");
+
+};
